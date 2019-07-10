2dd2634832e1ed425dd2baf28b976f7286c46540
Rajiv Ranjan <rajranja@redhat.com>
2019-07-10 17:30:23 +0530

    tested basic .net core app

---
+++ dotnet/myApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current date and time is: "+ DateTime.Now);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ dotnet/myWebApp/Pages/Index.cshtml
@@ -0,0 +1,8 @@
+﻿@page
+@model IndexModel
+@{
+    ViewData["Title"] = "Home page";
+}
+
+<h1>Hello, world!</h1>
+<p>The time on the server is @DateTime.Now</p>
